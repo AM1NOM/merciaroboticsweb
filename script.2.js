5759fd9df1c39e08fe1aec1e6d66a84a0fc8d779
AMiN <111236870+AM1NOM@users.noreply.github.com>
2025-11-17 00:27:57 +0330

    Update and rename style.2.js to script.2.js

--- script.2.js
+++ script.2.js
@@ -1,13 +1,3 @@
-// JavaScript Document
-
-/*
-
-TemplateMo 597 Neural Glass
-
-https://templatemo.com/tm-597-neural-glass
-
-*/
-
 // Mobile menu functionality
         const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
         const mobileNav = document.querySelector('.mobile-nav');
